3b06decabf8731ad21e0a5b6a0aea9bf6b64a056
EliseSugiyama <elise.sugiyama5@gmail.com>
2026-03-12 12:47:06 -0300

    Initial commit

---
+++ aula-12-03-2026/script.js
@@ -0,0 +1,56 @@
+// buscar o elemento HTML via javascript
+let meuElemento = document.getElementById("paragrafo"); // id
+console.log(meuElemento);
+console.log(meuElemento.textContent); // pegar o conteúdo da tag
+
+
+// buscar o elemento HTLM pela classe
+let paragrafo1 = document.getElementsByClassName("paragrafo");
+console.log(paragrafo1);
+for(let i=0; i<paragrafo1.length; i++){
+    console.log(paragrafo1[i].textContent);
+}
+
+
+// buscar o elemento pela tag
+let paragrafo2 = document.getElementsByTagName("p");
+console.log(paragrafo2);
+
+
+// criar o elemento HTML
+let destino = document.getElementById("elemento"); // buscando o elemento HTML
+let p = document.createElement("p"); // criar o elemento HTML
+p.textContent = "Paragrafo criado via JavaScript."; 
+destino.append(p); // adicionar ao DOM 
+
+
+// criar a nossa lista
+let lista = document.getElementById("lista"); // buscar o elemento HTML
+let ul = document.createElement("ul"); // cria o elemento HTML no javascript
+let itens = ["Arroz", "Feijão", "Suco"]; // adiciona os itens
+for(let i=0; i<itens.length; i++){
+    let li = document.createElement("li"); // cria o elemento da lista
+    li.textContent = itens[i]; // adiciona o conteúdo da lista ao elemento LI 
+    ul.append(li); // junta o LI ao UL 
+}
+lista.append(ul); // adiciona a UL ao DOM (página HTML)
+
+
+// função somar
+function somar(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value); 
+    let soma = n1 + n2;
+    // console.log(soma);
+    let saida = `Resultado: <b>${soma}</b>`;
+    document.getElementById("resultado").innerHTML = saida; 
+}
+
+
+// eventos
+let botao = document.getElementById("botao1");
+botao.onclick = function(){
+    alert("Clicou!");
+    botao.textContent = "Você clicou!";
+    botao.style.backgroundColor = "red";
+}
